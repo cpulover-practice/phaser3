f9457b0c2d4ff46e49ad8f7fe105b010773221ea
Hieu Ngo <justacpulover@gmail.com>
2020-09-16 17:53:58 +0200

    Load assets into scene

--- src/scenes/BootGameScene.ts
+++ src/scenes/BootGameScene.ts
@@ -7,14 +7,21 @@ export default class BootGameScene extends Phaser.Scene {
 
     // load assets: images, audio, etc.
     preload() {
+        // static images
+        this.load.image('sky', 'assets/sky.png')
+        this.load.image('platform', 'assets/platform.png')
+        this.load.image('star', 'assets/star.png')
+        this.load.image('bomb', 'assets/bomb.png')
+        // sprite image for animation
+        this.load.spritesheet('dude', 'assets/dude.png', {
+            frameWidth: 32,
+            frameHeight: 48
+        })
     }
 
     // add objects to the scene
-    create() {
-        this.add.text(0, 0, "Loading game...")
-    }
+    create() {}
 
     // loop
-    update() {
-    }
+    update() {}
 }
