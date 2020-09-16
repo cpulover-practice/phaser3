44afb6fcf9fe8993dd838493442403825f894d5f
Hieu Ngo <justacpulover@gmail.com>
2020-09-16 23:40:18 +0200

    Add player with collision

--- src/scenes/BootGameScene.ts
+++ src/scenes/BootGameScene.ts
@@ -2,7 +2,9 @@ import Phaser from 'phaser'
 import * as main from 'main'
 
 export default class BootGameScene extends Phaser.Scene {
-    private platforms ? : Phaser.Physics.Arcade.StaticGroup;
+    private platforms ? : Phaser.Physics.Arcade.StaticGroup; // "?" means attribute could be undefined
+    private player ? : Phaser.Physics.Arcade.Sprite;
+
     constructor() {
         super('boot-game')
     }
@@ -25,12 +27,19 @@ export default class BootGameScene extends Phaser.Scene {
     create() {
         this.add.image(0, 0, 'sky').setOrigin(0, 0) // reset the drawing position of the image to the top-left        
 
+        // platforms
         this.platforms = this.physics.add.staticGroup()
         const ground = this.platforms.create(400, 568, 'platform') as Phaser.Physics.Arcade.Image
         ground.setScale(2).refreshBody()
         this.platforms.create(600, 400, 'platform')
         this.platforms.create(50, 250, 'platform')
         this.platforms.create(750, 220, 'platform')
+
+        // player
+        this.player = this.physics.add.sprite(100, 450, 'dude')
+        this.player.setBounce(0.3)
+        this.player.setCollideWorldBounds(true)
+        this.physics.add.collider(this.player, this.platforms)
     }
 
     // loop
